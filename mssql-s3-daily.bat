ea00264c82387bba1503e34ddfa0da62f516435f
ds.seo <ssolphanes@naver.com>
2021-03-12 14:16:43 +0900

    feat: 시간기준 변경, 경로 세분화

--- mssql-s3-daily.bat
+++ mssql-s3-daily.bat
@@ -1,6 +1,7 @@
 @echo off
 for /f %%a in ('D:\bems-to-s3\yesterday.bat') do set "ystd=%%a"
-
-sqlcmd -S localhost -d iBems_SU -W -Q "SET NOCOUNT ON; SELECT * FROM iBems_SU.dbo.BemsMonitoringPointHistoryDaily WHERE SiteId=1 AND CreatedDateTime between CONCAT(CONVERT(char(10), dateadd(day, -1, getdate()), 23), ' 06:00') and CONCAT(CONVERT(char(10), dateadd(day, -0, getdate()), 23), ' 06:00')" -s ", " -o "C:\Users\Temp\%ystd%_BemsMonitoringPointHistoryDaily.csv"
-
-aws s3 cp "C:\Users\Temp\%ystd%_BemsMonitoringPointHistoryDaily.csv" s3://hdci-ambt-raw/dev/bems/db=ambt_dbo_tmp/tb=bemsmonitoringpointhistorydaily/
+set year=%ystd:~0,4%
+set mm=%ystd:~4,2%
+set dd=%ystd:~-2%
+sqlcmd -S localhost -d iBems_SU -W -Q "SET NOCOUNT ON; SELECT * FROM iBems_SU.dbo.BemsMonitoringPointHistoryDaily WHERE SiteId=1 AND CreatedDateTime between CONCAT(CONVERT(char(10), dateadd(day, -1, getdate()), 23), ' 00:00') and CONCAT(CONVERT(char(10), dateadd(day, -1, getdate()), 23), ' 23:59')" -s ", " -o "C:\Users\Temp\%ystd%_BemsMonitoringPointHistoryDaily.csv"
+aws s3 cp "C:\Users\Temp\%ystd%_BemsMonitoringPointHistoryDaily.csv" s3://hdci-ambt-raw/dev/bems/db=ambt_file/tb=bemsmonitoringpointhistorydaily/year=%year%/month=%mm%/day=%dd%/
